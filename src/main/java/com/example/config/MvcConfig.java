ea16a5914274d449786209d888e57057f50122c9
Ilya Rogov <mr.allcoder@gmail.com>
2019-12-18 02:07:11 +0300

    Finally add CSS support

--- src/main/java/com/example/config/MvcConfig.java
+++ src/main/java/com/example/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.example.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +10,18 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+//    public void addResourceHandlers(final ResourceHandlerRegistry registry){
+//        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+//    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/img/**")
+//                .addResourceLocations("file://" + uploadPath + "/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 
 }
