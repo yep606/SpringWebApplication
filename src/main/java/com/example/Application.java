406a8cb9ba5558d6fee6c2c8636434c44f935167
yep606 <ilya.rogov.vladislav@gmail.com>
2020-02-27 23:16:03 +0300

    Fix

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ *
  * @author yep606
+ *
  */
 
 @SpringBootApplication
