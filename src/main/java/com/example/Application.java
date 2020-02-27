aa1253fbb5881939b9eff17f35a1036a59fd5bbf
khrnt <Tanya51rus@gmail.com>
2020-02-27 23:11:25 +0300

    Fix

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,6 +3,10 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yep606
+ */
+
 @SpringBootApplication
 public class Application {
 
