5b58ac62142a006009c7432cb5e2764de83f016b
yep606 <ilya.rogov.vladislav@gmail.com>
2020-03-14 19:42:47 +0300

    Enabled rememberMe

--- src/main/java/com/example/config/WebSecurityConfig.java
+++ src/main/java/com/example/config/WebSecurityConfig.java
@@ -41,6 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .loginPage("/login")
                     .permitAll()
                 .and()
+                    .rememberMe().and()
                     .logout()
                     .permitAll();
     }
